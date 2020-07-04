e6534c1790a17e1607b6ce54bcb8e44f0f0f8e1b
Noah Gift <noah.gift@gmail.com>
2020-07-04 13:24:07 -0400

    ready for auto-deploy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -13,6 +14,11 @@ def name(value):
     print(f"You passed in this variable into the url: {value}")
     val = {"value": value}
     return jsonify(val)
+
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
