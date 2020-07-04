1fc95082b4eb4cd5a089e7564b642ee66b5f62ce
Noah Gift <noah.gift@gmail.com>
2020-07-04 13:17:14 -0400

    adding sample

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello I like to make AI Apps'
+
+@app.route('/name/<value>')
+def name(value):
+    print(f"You passed in this variable into the url: {value}")
+    val = {"value": value}
+    return jsonify(val)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
